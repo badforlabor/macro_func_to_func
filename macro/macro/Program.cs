194ff934a8004b2c8204ad5cfc72dd2e1d6cc410
bad-labor <bad-labor@home.com>
2017-02-24 00:05:35 +0800

    简单实现

---
+++ macro/macro/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace macro
+{
+    class Program
+    {
+        static string Process(string plain, string key, string value)
+        {
+            string ret = plain;
+
+            ret = ret.Replace(string.Format("##{0}", key), value);
+            //ret = ret.Replace(string.Format("#{0}", key), string.Format("\"{0}\"", key));
+            ret = ret.Replace(key, value);
+
+            return ret;
+        }
+
+        static void Main(string[] args)
+        {
+            string plain = File.ReadAllText("1.txt");
+
+            plain = Process(plain, "signx", "+");
+            plain = Process(plain, "signy", "+");
+            plain = Process(plain, "rx", "x");
+            plain = Process(plain, "ry", "y");
+            plain = Process(plain, "nx", "p");
+            plain = Process(plain, "ny", "p");
+            plain = Process(plain, "nf", "n");
+            plain = Process(plain, "apply_edge", "true");
+            plain = Process(plain, "apply_diag", "true");
+            plain = Process(plain, "\\", "");
+
+
+            File.WriteAllText("2.txt", plain);
+        }
+    }
+}
